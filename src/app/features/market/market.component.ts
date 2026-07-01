8e95ed897b697dde887eeb73ee822eaf63822ee9
Wesley McCammon <wesleymccammon@hotmail.com>
2026-07-01 12:23:03 -0500

    Add filtering to market overview

--- src/app/features/market/market.component.ts
+++ src/app/features/market/market.component.ts
@@ -2,6 +2,8 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
 import { InstrumentService } from '../../core/services/instrument.service';
+import { CategoryService } from '../../core/services/category.service';
+import { InstrumentCategory } from '../../shared/model/instrument.model';
 import { InstrumentCardComponent } from '../../shared/components/instrument-card/instrument-card.component';
 
 @Component({
@@ -13,24 +15,54 @@ import { InstrumentCardComponent } from '../../shared/components/instrument-card
 })
 export class MarketComponent {
   private instrumentService = inject(InstrumentService);
+  private categoryService   = inject(CategoryService);
 
   instruments = this.instrumentService.instruments;
+
+  readonly futuresCategories: InstrumentCategory[] = this.categoryService.byType('futures');
+  readonly forexCategories:   InstrumentCategory[] = this.categoryService.byType('forex');
 
   showFutures = signal(true);
   showForex   = signal(true);
   showCfds    = signal(true);
   showMicro   = signal(true);
+  activeF     = signal<Set<InstrumentCategory>>(new Set(this.futuresCategories));
+  activeFx    = signal<Set<InstrumentCategory>>(new Set(this.forexCategories));
 
   futures = computed(() => {
-    const micro = this.showMicro();
+    const active = this.activeF();
+    const micro  = this.showMicro();
     return this.instruments().filter(i =>
-      i.type === 'futures' && (micro || !i.name.startsWith('Micro'))
+      i.type === 'futures' &&
+      active.has(i.category) &&
+      (micro || !i.name.startsWith('Micro'))
     );
   });
-  forex = computed(() => this.instruments().filter(i => i.type === 'forex'));
-  cfds  = computed(() => this.instruments().filter(i => i.type === 'cfd'));
+
+  forex = computed(() => {
+    const active = this.activeFx();
+    return this.instruments().filter(i => i.type === 'forex' && active.has(i.category));
+  });
+
+  cfds = computed(() => this.instruments().filter(i => i.type === 'cfd'));
 
   constructor() {
     inject(Title).setTitle('Market – MarketWatch');
+  }
+
+  toggleFutures(cat: InstrumentCategory): void {
+    this.activeF.update(set => {
+      const next = new Set(set);
+      next.has(cat) ? next.delete(cat) : next.add(cat);
+      return next;
+    });
+  }
+
+  toggleForex(cat: InstrumentCategory): void {
+    this.activeFx.update(set => {
+      const next = new Set(set);
+      next.has(cat) ? next.delete(cat) : next.add(cat);
+      return next;
+    });
   }
 }
